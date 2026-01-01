173ad4dc0ac3b3b20ea0bbd632e0d9d0bd16cd5d
derrxme <deriansyahgithub@gmail.com>
2026-01-02 01:30:28 +0800

    day 1

---
+++ asset/script.js
@@ -0,0 +1,9 @@
+// coding
+
+let parrent = document.querySelector('.all-nutrisi')
+let protein = document.querySelector('.protein')
+let karbo = document.querySelector('.karbohidrat')
+
+parrent.addEventListener('mouseenter', function(){
+    protein.click()
+})
